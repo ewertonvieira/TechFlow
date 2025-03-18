8141e06a54e2e9d90a51d7ca66f55cbbe05a8033
Willi Santoos <willisantoos@gmail.com>
2025-03-18 11:09:08 -0300

    model midia criado

---
+++ app/Models/Midia.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Datababe\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Midia extends Model
+{
+    protected $table = 'midia';
+
+    protected $fillable =['midia'];
+}
